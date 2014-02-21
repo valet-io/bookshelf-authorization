4bad097a222904d4f7ac8e6a83806be8c6fadc6c
Ben Drucker <bvdrucker@gmail.com>
2014-02-21 14:56:55 -0500

    Gulp

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+
+gulp.task('lint', function () {
+  return gulp.src(['lib/*.js', 'test/**/*.js'])
+    .pipe(plugins.jshint())
+    .pipe(plugins.jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('cover', function () {
+  return gulp.src('lib/*.js')
+    .pipe(plugins.istanbul());
+});
+
+gulp.task('test', ['cover'], function () {
+  return gulp.src('test/*.js')
+    .pipe(plugins.mocha())
+    .pipe(plugins.istanbul.writeReports());
+});
