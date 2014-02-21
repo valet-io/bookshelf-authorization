32b02687bf872dcf1ec66ce8cee249fe6deac253
Ben Drucker <bvdrucker@gmail.com>
2014-02-21 18:02:19 -0500

    Integration test structure for configuration api/objects

---
+++ test/integration/configuration.js
@@ -0,0 +1,17 @@
+var User = require('../../lib/user')(require('../mocks/model'));
+
+describe('Integration: Configuration', function () {
+
+  describe('Configuration API', function () {
+
+    it('can configure an operation', function () {
+
+    });
+
+  });
+
+  describe('Configuration Object', function () {
+
+  });
+
+});
