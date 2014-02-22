57d5a748cbc14e84fc1b9186b43a7edf4884461d
Ben Drucker <bvdrucker@gmail.com>
2014-02-21 22:26:01 -0500

    Extremely brittle builder implementation, but functional. Needs refactoring to more complex rule structure.

--- test/integration/configuration.js
+++ test/integration/configuration.js
@@ -1,16 +1,32 @@
-var User = require('../../lib/user')(require('../mocks/model'));
+var expect    = require('chai').expect;
+var _         = require('lodash');
+var ModelBase = require('../../lib/model')(require('../mocks/model'));
 
-describe('Integration: Configuration', function () {
+describe('Integration: Configuration API', function () {
 
-  describe('Configuration API', function () {
-
-    it('can configure an operation', function () {
-
-    });
-
+  var Model, fn;
+  beforeEach(function () {
+    Model = ModelBase.extend();
+    fn = function () {};
   });
 
-  describe('Configuration Object', function () {
+  describe('Operation', function () {
+
+    it('can authorize an operation', function () {
+      Model.prototype.authorization.read = false;
+      Model.authorize('read').always();
+      expect(Model.prototype.authorization.read).to.be.true;
+    });
+
+    it('can prevent an operation', function () {
+      Model.authorize('read').never();
+      expect(Model.prototype.authorization.read).to.be.false;
+    });
+
+    it('can define an authorization handler', function () {
+      Model.authorize('read').when(fn);
+      expect(Model.prototype.authorization.read).to.equal(fn);
+    });
 
   });
 
