bcc62701f2769fbdc9813777745ce76e32311981
Ben Drucker <bvdrucker@gmail.com>
2014-02-24 18:21:55 -0500

    Change nomenclature for rules to Rule/test + #eval

--- test/unit/rule.js
+++ test/unit/rule.js
@@ -9,83 +9,83 @@ describe('Rule', function () {
 
   describe('constructor', function () {
 
-    it('accepts boolean rules', function () {
-      expect(new Rule(false)).to.have.property('rule', false);
+    it('accepts boolean tests', function () {
+      expect(new Rule(false)).to.have.property('test', false);
     });
 
-    it('accepts function rules', function () {
+    it('accepts function tests', function () {
       var fn = function () {};
-      expect(new Rule(fn)).to.have.property('rule', fn);
+      expect(new Rule(fn)).to.have.property('test', fn);
     });
 
-    it('rejects invalid rules', function () {
+    it('rejects invalid tests', function () {
       expect(function () {
         new Rule(null);
-      }).to.throw(/Invalid authorization rule/);
+      }).to.throw(/Invalid authorization test/);
     });
 
-    describe('#type', function () {
+  });
 
-      it('returns the rule type', function () {
-        expect(new Rule(true)).to.have.property('type', 'boolean');
-      });
+  describe('#type', function () {
 
+    it('returns the test type', function () {
+      expect(new Rule(true)).to.have.property('type', 'boolean');
     });
 
-    describe('#test', function () {
+  });
 
-      it('resolves when rule === true', function () {
-        return new Rule(true).test();
+  describe('#eval', function () {
+
+    it('resolves when test === true', function () {
+      return new Rule(true).eval();
+    });
+
+    it('rejects when test === false', function () {
+      return expect(new Rule(false).eval())
+        .to.be.rejectedWith(AuthorizationError);
+    });
+
+    describe('test = fn', function () {
+
+      it('rejects when fn() === false', function () {
+        return expect(new Rule(function () {
+          return false;
+        }).eval())
+        .to.be.rejectedWith(AuthorizationError);
       });
 
-      it('rejects when rule === false', function () {
-        return expect(new Rule(false).test())
-          .to.be.rejectedWith(AuthorizationError);
+      it('resolves when fn() == true', function () {
+        return new Rule(function () {
+          return true;
+        })
+        .eval()
+        .then(function (value) {
+          expect(value).to.be.null;
+        });
       });
 
-      describe('rule = fn', function () {
-
-        it('rejects when fn() === false', function () {
-          return expect(new Rule(function () {
-            return false;
-          }).test())
-          .to.be.rejectedWith(AuthorizationError);
+      it('resolves when fn() is a resolved promise', function () {
+        return new Rule(function () {
+          return Promise.resolve('resolution');
+        })
+        .eval()
+        .then(function (value) {
+          expect(value).to.be.null;
         });
+      });
 
-        it('resolves when fn() == true', function () {
-          return new Rule(function () {
-            return true;
-          })
-          .test()
-          .then(function (value) {
-            expect(value).to.be.null;
-          });
-        });
+      it('rejects when fn() is a rejected promise', function () {
+        return expect(new Rule(function () {
+          return Promise.reject(new Error('reason'));
+        }).eval())
+        .to.be.rejectedWith(AuthorizationError, 'reason');
+      });
 
-        it('resolves when fn() is a resolved promise', function () {
-          return new Rule(function () {
-            return Promise.resolve('resolution');
-          })
-          .test()
-          .then(function (value) {
-            expect(value).to.be.null;
-          });
-        });
-
-        it('rejects when fn() is a rejected promise', function () {
-          return expect(new Rule(function () {
-            return Promise.reject(new Error('reason'));
-          }).test())
-          .to.be.rejectedWith(AuthorizationError, 'reason');
-        });
-
-        it('rejects when fn throws', function () {
-          return expect(new Rule(function () {
-            throw new Error('reason');
-          }).test())
-          .to.be.rejectedWith(AuthorizationError, 'reason');
-        });
-
+      it('rejects when fn throws', function () {
+        return expect(new Rule(function () {
+          throw new Error('reason');
+        }).eval())
+        .to.be.rejectedWith(AuthorizationError, 'reason');
       });
 
     });
