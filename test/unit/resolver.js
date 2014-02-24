d002efabfe8deb541a133c7874ff66a2abe8efbe
Ben Drucker <bvdrucker@gmail.com>
2014-02-24 18:44:13 -0500

    Clean up resolver for easier testing

--- test/unit/resolver.js
+++ test/unit/resolver.js
@@ -4,33 +4,37 @@ var expect    = require('chai').expect;
 var sinon     = require('sinon');
 var ModelBase = require('../mocks/model');
 var UserBase  = require('../mocks/user');
-var resolve   = require('../../lib/resolve')(ModelBase);
+var resolver  = require('../../lib/resolver')(ModelBase);
 
 describe('Rule Resolver', function () {
 
-  it('resolves the authorization tests for a given user, method, target', function () {
-    var Model = ModelBase.extend({
-      authorization: [{method: 'write', user: UserBase}]
+  describe('#resolve', function () {
+
+    it('resolves the authorization tests for a given user, method, target', function () {
+      var Model = ModelBase.extend({
+        authorization: [{method: 'write', user: UserBase}]
+      });
+      expect(resolver.resolve(UserBase, 'write', Model))
+        .to.have.length(1);
     });
-    expect(resolve(UserBase, 'write', Model))
-      .to.have.length(1);
+
   });
 
   describe('#authorization', function () {
 
     it('returns the authorization from a model instance', function () {
       var target = new ModelBase();
-      expect(resolve.authorization(target)).to.equal(target.authorization);
+      expect(resolver.authorization(target)).to.equal(target.authorization);
     });
 
     it('creates a constructor to access authorization on a ctor', function () {
       var Target = sinon.spy(ModelBase.extend());
-      expect(resolve.authorization(Target)).to.equal(Target.prototype.authorization);
+      expect(resolver.authorization(Target)).to.equal(Target.prototype.authorization);
       sinon.assert.calledWithNew(Target);
     });
 
     it('throws if the target is not an instance or ctor', function () {
-      expect(resolve.authorization).to.throw(/Invalid authorization target/);
+      expect(resolver.authorization.bind(resolver)).to.throw(/Invalid authorization target/);
     });
 
   });
@@ -38,15 +42,15 @@ describe('Rule Resolver', function () {
   describe('#userCtor', function () {
 
     it('returns the constructor for an instance', function () {
-      expect(resolve.userCtor(new UserBase())).to.equal(UserBase);
+      expect(resolver.userCtor(new UserBase())).to.equal(UserBase);
     });
 
     it('returns the input if it is a constructor', function () {
-      expect(resolve.userCtor(UserBase)).to.equal(UserBase);
+      expect(resolver.userCtor(UserBase)).to.equal(UserBase);
     });
 
     it('throws if the user is not an instance or ctor', function () {
-      expect(resolve.userCtor).to.throw(/Invalid user/);
+      expect(resolver.userCtor.bind(resolver)).to.throw(/Invalid user/);
     });
 
   });
@@ -59,22 +63,22 @@ describe('Rule Resolver', function () {
     });
 
     it('includes items with matching users', function () {
-      expect(resolve.rules([{user: User}], User))
+      expect(resolver.rules([{user: User}], User))
         .to.have.length(1);
     });
 
     it('excludes items with non-matching users', function () {
-      expect(resolve.rules([{user: User}], User2))
+      expect(resolver.rules([{user: User}], User2))
         .to.be.empty;
     });
 
     it('includes items with matching methods', function () {
-      expect(resolve.rules([{method: 'write'}], undefined, 'write'))
+      expect(resolver.rules([{method: 'write'}], undefined, 'write'))
         .to.have.length(1);
     });
 
     it('excludes items with non-matching methods', function () {
-      expect(resolve.rules([{method: 'write'}], undefined, 'read'))
+      expect(resolver.rules([{method: 'write'}], undefined, 'read'))
         .to.be.empty;
     });
 
@@ -83,12 +87,12 @@ describe('Rule Resolver', function () {
   describe('#isModel', function () {
 
     it('checks whether the input is a model instance', function () {
-      expect(resolve.isModel(ModelBase)).to.be.false;
-      expect(resolve.isModel(new ModelBase())).to.be.true;
+      expect(resolver.isModel(ModelBase)).to.be.false;
+      expect(resolver.isModel(new ModelBase())).to.be.true;
     });
 
     it('can handle empty input', function () {
-      expect(resolve.isModel).to.not.throw();
+      expect(resolver.isModel).to.not.throw();
     });
 
   });
@@ -96,12 +100,12 @@ describe('Rule Resolver', function () {
   describe('#isCtor', function () {
 
     it('checks whether the input is a model constructor', function () {
-      expect(resolve.isCtor(ModelBase)).to.be.true;
-      expect(resolve.isCtor(new ModelBase())).to.be.false;
+      expect(resolver.isCtor(ModelBase)).to.be.true;
+      expect(resolver.isCtor(new ModelBase())).to.be.false;
     });
 
     it('can handle empty input', function () {
-      expect(resolve.isCtor).to.not.throw();
+      expect(resolver.isCtor).to.not.throw();
     });
 
   });
