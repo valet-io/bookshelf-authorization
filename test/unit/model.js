0ad0f4cbbbb6a2ef8b3ada9d5c2d1fa04f83db0a
Ben Drucker <bvdrucker@gmail.com>
2014-02-25 13:32:14 -0500

    Model features

--- test/unit/model.js
+++ test/unit/model.js
@@ -1,18 +1,53 @@
-var expect = require('chai').expect;
-var Model  = require('../mocks/model');
+'use strict';
 
-describe('Resource', function () {
-  'use strict';
+var expect    = require('chai').expect;
+var sinon     = require('sinon');
+var ModelBase = require('../mocks/model');
+var UserBase  = require('../mocks/user');
+var Builder   = require('../../lib/builder');
 
-  var model;
+describe('Model', function () {
+
+  var Model;
   beforeEach(function () {
-    model = new Model();
+    Model = ModelBase.extend();
   });
 
   describe('.authorization', function () {
 
-    it('defaults to full authorization', function () {
-      expect(model).to.have.property('authorization')
+    describe('.rules', function () {
+
+      it('is empty by default', function () {
+        expect(Model).to.have.deep.property('authorization.rules')
+          .that.is.empty;
+      });
+
+    });
+
+  });
+
+  describe('#authorize', function () {
+
+    it('returns a new rule builder', function () {
+      expect(Model.authorize(UserBase)).to.be.an.instanceOf(Builder);
+    });
+
+    it('calls the builder with the User and rule target', function () {
+      sinon.spy(Builder, 'create');
+      Model.authorize(UserBase);
+      expect(Builder.create).to.have.been.calledWith(UserBase, Model.authorization.rules);
+    });
+
+  });
+
+  describe('#extend', function () {
+
+    it('resets the authorization', function () {
+      var Parent = Model.extend();
+      Parent.authorization.rules.push();
+      var Child = Parent.extend();
+      expect(Child)
+        .to.have.deep.property('authorization.rules')
         .that.is.empty;
     });
 
