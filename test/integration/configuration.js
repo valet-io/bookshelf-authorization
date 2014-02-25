9db8a0f7107e7e65ffbdc1337a75208ac44d3c1c
Ben Drucker <bvdrucker@gmail.com>
2014-02-25 17:23:02 -0500

    Top, User, Method Level config API integration test

--- test/integration/configuration.js
+++ test/integration/configuration.js
@@ -9,28 +9,97 @@ describe('Integration: Configuration API', function () {
   beforeEach(function () {
     Model = ModelBase.extend();
     User = UserBase.extend();
+    fn = function () {};
+  });
+
+  var rules;
+  beforeEach(function () {
+    rules = function () {
+      return Model.authorization.rules;
+    };
+
+    rules.ensureOne = function () {
+      expect(rules()).to.have.length(1);
+    };
+
+    Object.defineProperty(rules, 'first', {
+      enumerable: true,
+      get: function () {
+        return rules()[0];
+      }
+    });
+  });
+
+  afterEach(function () {
+    rules.ensureOne();
+  });
+
+  describe('Top Level', function () {
+
+    it('can authorize everything', function () {
+      Model.authorize.always();
+      expect(rules.first).to.have.property('test', true);
+    });
+
+    it('can prevent everything', function () {
+      Model.authorize.never();
+      expect(rules.first).to.have.property('test', false);
+    });
+
+    it('can validate everything with a function', function () {
+      Model.authorize.when(fn);
+      expect(rules.first).to.have.property('test', fn);
+    });
+
   });
 
   describe('User', function () {
 
-    it('can authorize a user to do anything', function () {
-      Model.authorize(User).always();
-      var rule = Model.authorization.rules[0];
-      expect(rule).to.have.property('test', true);
-      expect(rule).to.have.property('user', User);
+    afterEach(function () {
+      expect(rules.first).to.have.property('user', User);
+    });
+
+    it('can allow a user to do anything', function () {
+      Model.authorize.a(User).always();
+      expect(rules.first).to.have.property('test', true);
     });
 
     it('can prevent a user from doing anything', function () {
-      Model.authorize(User).never();
-      var rule = Model.authorization.rules[0];
-      expect(rule).to.have.property('test', false);
+      Model.authorize.a(User).never();
+      expect(rules.first).to.have.property('test', false);
     });
 
-    it('can validate all user actions with a function', function () {
-      var fn = function () {};
-      Model.authorize(User).when(fn);
-      var rule = Model.authorization.rules[0];
-      expect(rule).to.have.property('test', fn);
+    it('can validate user actions with a function', function () {
+      Model.authorize.a(User).when(fn);
+      expect(rules.first).to.have.property('test', fn);
+    });
+
+  });
+
+  describe('Method', function () {
+
+    afterEach(function () {
+      expect(rules.first).to.have.property('method', 'write');
+    });
+
+    it('can validate all uses of any method', function () {
+      Model.authorize.anyone.to('write').always();
+      expect(rules.first).to.have.property('test', true);
+    });
+
+    it('can prevent all uses of a method', function () {
+      Model.authorize.anyone.to('write').never();
+      expect(rules.first).to.have.property('test', false);
+    });
+
+    it('can validate uses of a method with a function', function () {
+      Model.authorize.anyone.to('write').when(fn);
+      expect(rules.first).to.have.property('test', fn);
+    });
+
+    it('can use the chained syntax for registered methods', function () {
+      Model.authorize.anyone.to.write.when(fn);
+      expect(rules.first).to.have.property('test', fn);
     });
 
   });
