8fa0c4232b0189f4525a1f2af35b290053b82609
Ben Drucker <bvdrucker@gmail.com>
2014-02-25 19:51:55 -0500

    Don't throw possibly unhandled rejections. These come from Can#do and are expected.
    This reverts commit d8874f8e4bdcc2b9d6b41cd280c70ff27289afb4.

--- test/setup.js
+++ test/setup.js
@@ -2,8 +2,4 @@ require('mocha-as-promised')();
 require('chai')
   .use(require('chai-as-promised'))
   .use(require('sinon-chai'));
-var Bluebird = require('bluebird');
-Bluebird.longStackTraces();
-Bluebird.onPossiblyUnhandledRejection(function(err, promise){
-  throw err;
-});
+require('bluebird').longStackTraces()
