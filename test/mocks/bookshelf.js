a03e8b4e43e9bfbdbf52638ad2bb21b1e5220f8d
Ben Drucker <bvdrucker@gmail.com>
2014-03-02 00:54:23 -0500

    Add collection to bookshelf mock (used by registry plugin)

--- test/mocks/bookshelf.js
+++ test/mocks/bookshelf.js
@@ -1,5 +1,6 @@
 module.exports = {
   Model: require('bookshelf/dialects/base/model').ModelBase,
+  Collection: require('bookshelf/dialects/base/collection').CollectionBase,
   plugin: function (plugin) {
     plugin(this);
     return this;
